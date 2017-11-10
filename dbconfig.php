6c795bb48e46aabb831a6422270b9facc9f21413
dehinde molade <dehinde007@gmail.com>
2017-11-10 21:25:25 +0100

    10Nov

--- dbconfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host='localhost';
-$db = 'sblog';
-$username = 'root';
-$password = 'ambition1';
-?>
--- public/index.php
+++
@@ -1,8 +0,0 @@
-<?php include "templates/header.php"; ?>
-
-<ul class="indexul">
-	<li><a href="create.php"><strong>Create a post</strong></a></li>
-	<li><a href="read.php"><strong>View all post</strong></a></li>
-</ul>
-
-<?php include "templates/footer.php"; ?>
