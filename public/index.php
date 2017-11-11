d165bdda0fc7f0e6ce17475865ff312412ad506e
dehinde molade <dehinde007@gmail.com>
2017-11-11 19:37:23 +0100

    pdo_seal

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php include "templates/header.php"; ?>
+
+<ul class="indexul">
+	<li><a href="create.php"><strong>Create a post</strong></a></li>
+	<li><a href="read.php"><strong>View all post</strong></a></li>
+</ul>
+
+<?php include "templates/footer.php"; ?>
