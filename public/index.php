5b6b81702b04cad9f2c70249f41191bf4b99b2b7
dehinde molade <dehinde007@gmail.com>
2017-11-13 18:13:15 +0100

    13b/11/17

--- public/index.php
+++ public/index.php
@@ -5,13 +5,13 @@
 <h1 class="indexh1">Simple Database App</h1><br>
 
 <script type='text/javascript'>
-function delete_post( id ){
+function delete_post( post_id ){
      
     var answer = confirm('Are you sure?');
     if (answer){
         // if user clicked ok, 
         // pass the id to delete.php and execute the delete query
-        window.location = 'delete.php?id=' + id;
+        window.location = 'delete.php?post_id=' + post_id;
     } 
 }
 </script>
@@ -24,7 +24,7 @@ $conn = new PDO($dsn, $user, $password);
 
   // set the PDO error mode to exception
    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   $stmt = $conn->prepare("SELECT id, title, content, tag FROM posts"); 
+   $stmt = $conn->prepare("SELECT post_id, title, content, tag FROM posts"); 
    $stmt->execute();
 
    $num = $stmt->rowCount();
@@ -56,19 +56,19 @@ if($num>0){
              
             // creating new table row per record
             echo "<br><tr>";
-                echo "<td>{$id}</td>";
+                echo "<td>{$post_id}</td>";
                 echo "<td>{$title}</td>";
                 echo "<td>{$content}</td>";
                 echo "<td>{$tag}</td>";
                 echo "<td>";
                     // read one record 
-                    echo "<a href='read.php?id={$id}' class='btn btn-info m-r-1em'>View </a>";
+                    echo "<a href='read.php?post_id={$post_id}' class='btn btn-info m-r-1em'>View </a>";
                      
                     // we will use this links on next part of this post
-                    echo "<a href='update.php?id={$id}' class='btn btn-primary m-r-1em'>Edit</a>";
+                    echo "<a href='update.php?post_id={$post_id}' class='btn btn-primary m-r-1em'>Edit</a>";
  
                     // we will use this links on next part of this post
-                    echo "<a href='#' onclick='delete_post({$id});'  class='btn btn-danger'>Delete</a>";
+                    echo "<a href='#' onclick='delete_post({$post_id});'  class='btn btn-danger'>Delete</a>";
                 echo "</td>";
             echo "</tr>";
         }
